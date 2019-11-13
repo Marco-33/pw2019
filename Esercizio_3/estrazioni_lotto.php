3e3c2096e86fc69a91ecb023684e9a95077326d2
Marco-33 <56446880+Marco-33@users.noreply.github.com>
2019-11-13 19:38:35 +0100

    Esercizio_3 funziona!!

---
+++ Esercizio_3/estrazioni_lotto.php
@@ -0,0 +1,24 @@
+<?php 
+    function ruota(){
+
+}?>
+
+<html>
+<body>
+<table border=2>
+<tr> <td><h2>Città</h2></td><td colspan=4><h2>Numeri</h2></td></tr>
+<tr> <td>Bari</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Cagliari</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Firenze</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Genova</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Milano</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Napoli</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Palermo</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Roma</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Torino</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Venezia</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+<tr> <td>Nazionale</td> <?php for($x=0; $x<5; $x++){?><td><?=rand(1,90)?></td><?php }?> </tr>
+</table>
+<body>
+</html>
+
